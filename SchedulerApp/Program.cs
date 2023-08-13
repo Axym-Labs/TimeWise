678d00dee0930da57497678803358f64fc3f5542
Davide Wiest <davide.wiest2@gmail.com>
2023-08-13 19:40:54 +0200

    set mudblazor to dark mode, comment out numericfield for work hours for now

--- SchedulerApp/Program.cs
+++ SchedulerApp/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddMudServices();
 //    config.SnackbarConfiguration.PreventDuplicates = false;
 //    config.SnackbarConfiguration.NewestOnTop = false;
 //    config.SnackbarConfiguration.ShowCloseIcon = true;
-//    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+//    config.SnackbarConfiguration.VisibleStateDuration = 8000;
 //    config.SnackbarConfiguration.HideTransitionDuration = 500;
 //    config.SnackbarConfiguration.ShowTransitionDuration = 500;
 //    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
