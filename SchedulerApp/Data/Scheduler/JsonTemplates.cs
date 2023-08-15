fe0a1ce8b38f51c4f402c13b1691abbe5839f387
Jonathan Schäfer <jonathan.schaefer_@outlook.de>
2023-08-16 00:44:14 +0200

    Updated to use async effectively. And renamed object

--- SchedulerApp/Data/Scheduler/JsonTemplates.cs
+++ SchedulerApp/Data/Scheduler/JsonTemplates.cs
@@ -5,34 +5,33 @@ namespace SchedulerApp.Data.Scheduler;
 public class RatingUser
 {
     public int Rating { get; set; }
-    public string Role { get; set; }
+    public string Role { get; set; } = string.Empty;
 }
 
-public class RootObject
+public class UserRatings
 {
     public int TotalUsers { get; set; }
-    public List<int> Ratings { get; set; }
-    public Dictionary<string, RatingUser> RatingUsers { get; set; }
+    public List<int> Ratings { get; set; } = new List<int>();
+    public Dictionary<string, RatingUser> RatingUsers { get; set; } = new Dictionary<string, RatingUser>();
 }
 
 
 public static class usageFileLoader
 {
-    public static RootObject LoadFile()
+    public static async Task<UserRatings> LoadFileAsync()
     {
         // TODO: Fix this
         string basePath = AppDomain.CurrentDomain.BaseDirectory;
-        Console.WriteLine(basePath);
         string fullPath = Path.Combine(basePath, "/static/content/usage.json");
-        string json = File.ReadAllText("static/content/usage.json");
-        return JsonConvert.DeserializeObject<RootObject>(json);
+        string json = await File.ReadAllTextAsync("static/content/usage.json");
+        return JsonConvert.DeserializeObject<UserRatings>(json)!;
     }
 
-    public static void SaveFile(RootObject ro)
+    public static async Task SaveFileAsync(UserRatings ro)
     {
         string basePath = AppDomain.CurrentDomain.BaseDirectory;
         string fullPath = Path.Combine(basePath, "/static/content/usage.json");
         string json = JsonConvert.SerializeObject(ro, Formatting.Indented);
-        File.WriteAllText("static/content/usage.json", json);
+        await File.WriteAllTextAsync("static/content/usage.json", json);
     }
 }
