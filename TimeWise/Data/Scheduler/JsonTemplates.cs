0f3170d0fa7a4206f56160e73a659edbf64b4829
Davide Wiest <davide.wiest2@gmail.com>
2023-08-23 19:44:00 +0200

    add json file to store results

--- TimeWise/Data/Scheduler/JsonTemplates.cs
+++ TimeWise/Data/Scheduler/JsonTemplates.cs
@@ -16,8 +16,68 @@ public class UserRatings
     public Dictionary<string, RatingUser> RatingUsers { get; set; } = new Dictionary<string, RatingUser>();
 }
 
+public class ScheduleResults
+{
+    public double IlpCostAvg { get; set; }
 
-public static class usageFileLoader
+    public int IlpCostNum { get; set; }
+
+    public double ComparisonCostAvg { get; set; }
+
+    public int ComparisonCostNum { get; set; }
+
+    public double IlpStrainAvg { get; set; }
+
+    public int IlpStrainNum { get; set; }
+
+    public double ComparisonStrainAvg { get; set; }
+
+    public int ComparisonStrainNum { get; set; }
+}
+
+public static class ResultsFileLoader
+{
+    public static async Task SaveComparisonResults(double Strain, double Cost)
+    {
+        ScheduleResults sr = await LoadFileAsync();
+        double Ds = (Strain - sr.ComparisonStrainAvg) / sr.ComparisonStrainNum;
+        double Dc = (Cost - sr.ComparisonCostAvg) / sr.ComparisonCostNum;
+
+        sr.ComparisonStrainAvg += Math.Round(Ds, 2, MidpointRounding.AwayFromZero);
+        sr.ComparisonCostAvg += Math.Round(Dc, 2, MidpointRounding.AwayFromZero);
+        await SaveFileAsync(sr);
+    }
+
+    public static async Task SaveIlpResults(double Strain, double Cost)
+    {
+        ScheduleResults sr = await LoadFileAsync();
+        double Ds = (Strain - sr.IlpStrainAvg) / sr.IlpStrainNum;
+        double Dc = (Cost - sr.IlpCostAvg) / sr.IlpCostNum;
+
+        sr.IlpStrainAvg += Math.Round(Ds, 2, MidpointRounding.AwayFromZero);
+        sr.IlpCostAvg += Math.Round(Dc, 2, MidpointRounding.AwayFromZero);
+        await SaveFileAsync(sr);
+    }
+
+    public static async Task<ScheduleResults> LoadFileAsync()
+    {
+        // TODO: Fix this
+        string basePath = AppDomain.CurrentDomain.BaseDirectory;
+        string fullPath = Path.Combine(basePath, "/static/content/scheduleResults.json");
+        string json = await File.ReadAllTextAsync("static/content/scheduleResults.json");
+        return JsonConvert.DeserializeObject<ScheduleResults>(json)!;
+    }
+
+    public static async Task SaveFileAsync(ScheduleResults sr)
+    {
+        string basePath = AppDomain.CurrentDomain.BaseDirectory;
+        string fullPath = Path.Combine(basePath, "/static/content/scheduleResults.json");
+        string json = JsonConvert.SerializeObject(sr, Formatting.Indented);
+        await File.WriteAllTextAsync("static/content/scheduleResults.json", json);
+    }
+}
+
+public static class UsageFileLoader
 {
     public static async Task<UserRatings> LoadFileAsync()
     {
