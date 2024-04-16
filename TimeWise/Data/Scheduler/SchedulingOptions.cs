8279bdd2c0cf333f2ab528d73ced38970a1da2ad
Jonathan Schäfer <jonathan.schaefer_@outlook.de>
2024-04-16 14:47:52 +0200

    `Renamed property in SchedulingOptions class`
    ` `
    
    `Renamed the property 'CapMaximumWorkingHoursConstraint' to 'MaximumWorkingHoursConstraint' in the 'SchedulingOptions' class. This change has been implemented in both the property declaration and in the constructor parameters and assignment.`

--- TimeWise/Data/Scheduler/SchedulingOptions.cs
+++ TimeWise/Data/Scheduler/SchedulingOptions.cs
@@ -4,20 +4,20 @@ public class SchedulingOptions{
     public bool StrainMinimizing { get; set; } = true;
     public bool EnsureQualifiedPersonnelConstraint { get; set; } = true;
     public bool NoDoubleShiftConstraint { get; set; } = true;
-    public bool CapMaximumWorkingHoursConstraint { get; set; } = true;
+    public bool MaximumWorkingHoursConstraint { get; set; } = true;
     public bool MinimumWorkingHoursConstraint { get; set; } = false;
 
     public SchedulingOptions() {}
     public SchedulingOptions(bool StrainMinimizing,
                              bool EnsureQualifiedPersonnelConstraint,
                              bool NoDoubleShiftConstraint,
-                             bool CapMaximumWorkingHoursConstraint,
+                             bool MaximumWorkingHoursConstraint,
                              bool MinimumWorkingHoursConstraint) 
     {
         this.StrainMinimizing = StrainMinimizing;
         this.EnsureQualifiedPersonnelConstraint = EnsureQualifiedPersonnelConstraint;
         this.NoDoubleShiftConstraint = NoDoubleShiftConstraint;
-        this.CapMaximumWorkingHoursConstraint = CapMaximumWorkingHoursConstraint;
+        this.MaximumWorkingHoursConstraint = MaximumWorkingHoursConstraint;
         this.MinimumWorkingHoursConstraint = MinimumWorkingHoursConstraint;
     }
 }
